cbd92f39155d964220d086cac8dfc5cabbdf2df7
Pierre <pierre.besson7@gmail.com>
2015-01-15 22:35:21 +0100

    Refactoring of the example structure.

---
+++ app/index.js
@@ -0,0 +1,22 @@
+var React = require('react');
+var MyComponent = React.createClass({
+  handleClick: function() {
+    // Explicitly focus the text input using the raw DOM API.
+    this.refs.myTextInput.getDOMNode().focus();
+  },
+  render: function() {
+    // The ref attribute adds a reference to the component to
+    // this.refs when the component is mounted.
+    return (
+      <div>
+      <input type="text" ref="myTextInput" value={this.props.name} />
+      <button value={this.props.name} onClick={this.handleClick} />
+      </div>
+    );
+  }
+});
+
+React.render(
+  <MyComponent name="pierre"/>,
+  document.getElementById('example')
+);
