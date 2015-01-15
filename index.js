baec78204ae0d57fe7e6e47f70de545cc62a8027
Pierre <pierre.besson7@gmail.com>
2015-01-15 22:28:53 +0100

    First working example.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-var React = require('react')
-
-var Hello = React.createClass({
-
+var React = require('react');
+var MyComponent = React.createClass({
+  handleClick: function() {
+    // Explicitly focus the text input using the raw DOM API.
+    this.refs.myTextInput.getDOMNode().focus();
+  },
   render: function() {
-    return <div>Hello, {this.props.name}!</div>
+    // The ref attribute adds a reference to the component to
+    // this.refs when the component is mounted.
+    return (
+      <div>
+        <input type="text" ref="myTextInput" value={this.props.name} />
+        <button value={this.props.name} onClick={this.handleClick} />
+      </div>
+    );
   }
 });
 
 React.render(
-  <Hello name="World" />,
-  document.getElementById('hello')
+  <MyComponent name="pierre"/>,
+  document.getElementById('example')
 );
