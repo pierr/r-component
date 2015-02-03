962afb762c0c17acc59ef516c8d84c890ba33d68
Pierre <pierre.besson7@gmail.com>
2015-02-03 08:50:31 +0100

    Update the getField function.

--- app/model/alerte.js
+++ app/model/alerte.js
@@ -1,16 +1,20 @@
+/*global Backbone, module, require*/
+var labels = require('../config/i18n').alerte;
 module.exports = Backbone.Model.extend({
-  "subject": "alerte",
+  subject: "alerte",
   metadata: require('../config/metadata').alerte,
   getField: function getFieldByName(name){
       return {
         name: name,
         value: this.get(name),
-        metdata: this.metadata[name]
+        metdata: this.metadata[name],
+        label: labels[name],
+        error: this.has('errors') && this.get('errors')[name] !== undefined ? this.get('errors')[name]: undefined
       };
   },
   toJSON: function(){
     return  merge(
-    {metadata: this.metadata},
+      {metadata: this.metadata},
       Backbone.Model.prototype.toJSON.call(this)
     );
   }
