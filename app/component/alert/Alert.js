8fbf059f3b1977a8547ca99650cac8a06e51a9df
Pierre Besson <pierre.besson7@gmail.com>
2015-02-03 18:53:24 +0100

    [component]

--- app/component/alert/Alert.js
+++ app/component/alert/Alert.js
@@ -1,7 +1,7 @@
 /**@jsx*/
 var React = require('react');
 var Input = require('./input');
-var $ = require('zepto');
+var fetch = require('node-fetch');
 module.exports = React.createClass({
   propTypes: {
     alert: React.PropTypes.object.isRequired
@@ -12,7 +12,10 @@ module.exports = React.createClass({
       }
   },
   handleBtnAcceptClick: function handleBtnAcceptClick(){
-    $.post( 'http://localhost:8080/alerts/'+ alert.id/ +'/accept', {id: alert.id});
+    fetch('http://httpbin.org/post', { method: 'POST', body: 'a=1' }).then(
+
+    );
+    //$.post( 'http://localhost:8080/alerts/'+ alert.id/ +'/accept', {id: alert.id});
   },
   render: function() {
     var alert = this.props.alert;
